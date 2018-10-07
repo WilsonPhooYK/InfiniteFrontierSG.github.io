158b65cc3770ff66e15aac65cd53e012a8872589
WilsonPhoo <wilson@policypal.com>
2018-10-08 02:38:46 +0800

    - Initial

---
+++ precache-manifest.522a32850c6b6317a34b2d639aced3d0.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "eef867f0b1eb3874cdbf50fb6ae2c68f",
+    "url": "/static/media/portfolio2.eef867f0.jpg"
+  },
+  {
+    "revision": "c3ec6e429baa5c7a32a7",
+    "url": "/static/css/main.9c8e270f.chunk.css"
+  },
+  {
+    "revision": "293e57c4f8f5c090de97643ab31db143",
+    "url": "/static/media/bg.293e57c4.png"
+  },
+  {
+    "revision": "1ce79ce937dede152446",
+    "url": "/static/js/1.1ce79ce9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "357ee8e47ffbba26121b8daef3e1a388",
+    "url": "/static/media/img1.357ee8e4.png"
+  },
+  {
+    "revision": "07a72c4d87119233c4bbf9e5341f0d56",
+    "url": "/static/media/img2.07a72c4d.png"
+  },
+  {
+    "revision": "a4280a6c5120ac75f2dd4a590e5789f8",
+    "url": "/static/media/portfolio1.a4280a6c.jpg"
+  },
+  {
+    "revision": "c3ec6e429baa5c7a32a7",
+    "url": "/static/js/main.c3ec6e42.chunk.js"
+  },
+  {
+    "revision": "da72373d6cadf692f00a23f8baf10c3b",
+    "url": "/static/media/portfolio3.da72373d.jpg"
+  },
+  {
+    "revision": "fc36e5107bcc2d873bcff66fe53aa883",
+    "url": "/static/media/portfolio4.fc36e510.jpg"
+  },
+  {
+    "revision": "ae034f1c7f5f21521ae21e506dadcffa",
+    "url": "/static/media/Mascot.ae034f1c.png"
+  },
+  {
+    "revision": "7d1736e2495f379c345dada836b9c2e4",
+    "url": "/static/media/ifLogo.7d1736e2.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1ce79ce937dede152446",
+    "url": "/static/css/1.319052ee.chunk.css"
+  },
+  {
+    "revision": "36c6c569a988b3e6d0e8ad678c6e63fe",
+    "url": "/index.html"
+  }
+];
