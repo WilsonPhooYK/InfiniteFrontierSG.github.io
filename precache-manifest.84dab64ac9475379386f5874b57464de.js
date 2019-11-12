be41d395fe411c0ef0960c3a429cc02b8270c2f8
WilsonPhoo <wilson@policypal.com>
2019-11-13 02:20:25 +0800

    Updated new web

---
+++ precache-manifest.84dab64ac9475379386f5874b57464de.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2ea6c0a4ac49c4516b58e10b96e22c6",
+    "url": "https://infinitefrontier.sg/index.html"
+  },
+  {
+    "revision": "92e1651197f6523d73cf",
+    "url": "https://infinitefrontier.sg/static/css/main.d204059d.chunk.css"
+  },
+  {
+    "revision": "c5ce62a21f794dcc6a2a",
+    "url": "https://infinitefrontier.sg/static/js/2.afedf0fe.chunk.js"
+  },
+  {
+    "revision": "92e1651197f6523d73cf",
+    "url": "https://infinitefrontier.sg/static/js/main.6ddffe21.chunk.js"
+  },
+  {
+    "revision": "cc15a3129f35801e7608",
+    "url": "https://infinitefrontier.sg/static/js/runtime-main.880174bc.js"
+  },
+  {
+    "revision": "229073e961d5eb92aa31d20f3e8ecada",
+    "url": "https://infinitefrontier.sg/static/media/footer-bg.229073e9.svg"
+  },
+  {
+    "revision": "24f7b0944e9e03a905f9d7701573b2cd",
+    "url": "https://infinitefrontier.sg/static/media/open-sans-v17-latin-300.24f7b094.woff2"
+  },
+  {
+    "revision": "505d96bd1954c3e75b4b096279c83741",
+    "url": "https://infinitefrontier.sg/static/media/open-sans-v17-latin-300.505d96bd.svg"
+  },
+  {
+    "revision": "73a1b98bb0336703d0801aa1c9e210e3",
+    "url": "https://infinitefrontier.sg/static/media/open-sans-v17-latin-300.73a1b98b.ttf"
+  },
+  {
+    "revision": "a7622f60c56ddd5301549a786b54e6e6",
+    "url": "https://infinitefrontier.sg/static/media/open-sans-v17-latin-300.a7622f60.woff"
+  },
+  {
+    "revision": "e348323f13ccb18b3b5a72c49f9357d9",
+    "url": "https://infinitefrontier.sg/static/media/open-sans-v17-latin-300.e348323f.eot"
+  },
+  {
+    "revision": "449d681cd6006390e1bee3c3a660430b",
+    "url": "https://infinitefrontier.sg/static/media/open-sans-v17-latin-600.449d681c.woff"
+  },
+  {
+    "revision": "819af3d3abdc9f135d49b80a91e2ff4c",
+    "url": "https://infinitefrontier.sg/static/media/open-sans-v17-latin-600.819af3d3.woff2"
+  },
+  {
+    "revision": "8f7128145a3c2b83923552a328df7cdc",
+    "url": "https://infinitefrontier.sg/static/media/open-sans-v17-latin-600.8f712814.ttf"
+  },
+  {
+    "revision": "b1249dd5f8788e2a52fd73570e827bbd",
+    "url": "https://infinitefrontier.sg/static/media/open-sans-v17-latin-600.b1249dd5.svg"
+  },
+  {
+    "revision": "ddacaf4a9db58cda27c76ce88c408546",
+    "url": "https://infinitefrontier.sg/static/media/open-sans-v17-latin-600.ddacaf4a.eot"
+  },
+  {
+    "revision": "0edb76284a7a0f8db4665b560ee2b48f",
+    "url": "https://infinitefrontier.sg/static/media/open-sans-v17-latin-700.0edb7628.woff2"
+  },
+  {
+    "revision": "1f85e92d8ff443980bc0f83ad7b23b60",
+    "url": "https://infinitefrontier.sg/static/media/open-sans-v17-latin-700.1f85e92d.woff"
+  },
+  {
+    "revision": "9f5a774538ba87b28526abd3e988eda8",
+    "url": "https://infinitefrontier.sg/static/media/open-sans-v17-latin-700.9f5a7745.svg"
+  },
+  {
+    "revision": "e5111caba5b811a73d995786db3c61ea",
+    "url": "https://infinitefrontier.sg/static/media/open-sans-v17-latin-700.e5111cab.ttf"
+  },
+  {
+    "revision": "fc0dbb2edfac9ef855bcc6acd9e3477d",
+    "url": "https://infinitefrontier.sg/static/media/open-sans-v17-latin-700.fc0dbb2e.eot"
+  },
+  {
+    "revision": "05ebdbe10796850f045fcd484f35788d",
+    "url": "https://infinitefrontier.sg/static/media/open-sans-v17-latin-800.05ebdbe1.woff"
+  },
+  {
+    "revision": "45c95bf967b1b8e9494465806fecc6f0",
+    "url": "https://infinitefrontier.sg/static/media/open-sans-v17-latin-800.45c95bf9.svg"
+  },
+  {
+    "revision": "67a97b77ffd3fc237ea97596d68c6763",
+    "url": "https://infinitefrontier.sg/static/media/open-sans-v17-latin-800.67a97b77.ttf"
+  },
+  {
+    "revision": "ab3e500e7375695d702cff19513e4470",
+    "url": "https://infinitefrontier.sg/static/media/open-sans-v17-latin-800.ab3e500e.woff2"
+  },
+  {
+    "revision": "b48d8065f1875ca85f5f694a0397857e",
+    "url": "https://infinitefrontier.sg/static/media/open-sans-v17-latin-800.b48d8065.eot"
+  },
+  {
+    "revision": "06b4bfda4e139eaf3ab9872a6d66f42f",
+    "url": "https://infinitefrontier.sg/static/media/open-sans-v17-latin-italic.06b4bfda.woff"
+  },
+  {
+    "revision": "099f3e08e06bb013cd509183eb4446af",
+    "url": "https://infinitefrontier.sg/static/media/open-sans-v17-latin-italic.099f3e08.eot"
+  },
+  {
+    "revision": "427628806fa443fac21286fb154def5f",
+    "url": "https://infinitefrontier.sg/static/media/open-sans-v17-latin-italic.42762880.svg"
+  },
+  {
+    "revision": "a038c4d564d9c354a6c45a1da0cc414c",
+    "url": "https://infinitefrontier.sg/static/media/open-sans-v17-latin-italic.a038c4d5.ttf"
+  },
+  {
+    "revision": "b61a9055d92ac56c15cacf4271d4f072",
+    "url": "https://infinitefrontier.sg/static/media/open-sans-v17-latin-italic.b61a9055.woff2"
+  },
+  {
+    "revision": "049a929c5d81988b3ae6d2f985ca7aa5",
+    "url": "https://infinitefrontier.sg/static/media/open-sans-v17-latin-regular.049a929c.ttf"
+  },
+  {
+    "revision": "33543c5cc5d88f5695dd08c87d280dfd",
+    "url": "https://infinitefrontier.sg/static/media/open-sans-v17-latin-regular.33543c5c.woff2"
+  },
+  {
+    "revision": "5f4d4bc11d64b6cb605b7030c1997270",
+    "url": "https://infinitefrontier.sg/static/media/open-sans-v17-latin-regular.5f4d4bc1.eot"
+  },
+  {
+    "revision": "ddaaf4901aa3a2fa8805a235a7790f1f",
+    "url": "https://infinitefrontier.sg/static/media/open-sans-v17-latin-regular.ddaaf490.svg"
+  },
+  {
+    "revision": "de0869e324680c99efa1250515b4b41c",
+    "url": "https://infinitefrontier.sg/static/media/open-sans-v17-latin-regular.de0869e3.woff"
+  }
+]);
